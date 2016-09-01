44a52a17d58ad09c7e68b51b3d280f61d52b096d
Erin Gunn <ikonone@gmail.com>
2016-08-31 20:09:55 -0500

    Removed password_hash from the User model

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20160831024426) do
     t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "password_hash"
     t.string   "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
